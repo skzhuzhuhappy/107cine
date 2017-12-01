89f30774bffbaaf2247c1ee95932210317666401
gaoxing-vagrant <skzhuzhu15201255789@gmail.com>
2017-12-01 08:16:44 +0000

    接口

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,11 @@
 |
 */
 Route::get('/', 'Service\MainController@index');
-Route::get('/ppxy/index', 'Service\PpxyController@index');
-Route::get('/ppxy/main_info', 'Service\PpxyController@main_info');
-Route::get('/ppxy/find_json', 'Service\PpxyController@find_json');
-Route::get('/ppxy/platform/{type}', 'Service\PpxyController@platform');
-
-Route::get('/ppxy/course/{main_id}/{sub_id}','Service\PpxyController@course', function ($main_id,$sub_id) {
+Route::get('/ppxy/index', 'Service\PpxysController@index');
+Route::get('/ppxy/main_info', 'Service\PpxysController@main_info');
+Route::get('/ppxy/find_json', 'Service\PpxysController@find_json');
+Route::get('/ppxy/platform/{type}', 'Service\PpxysController@platform');
+Route::get('/ppxy/course/{main_id}/{sub_id}','Service\PpxysController@course', function ($main_id,$sub_id) {
 
 })->where(['main_id' => '[0-9]+', 'sub_id' => '[0-9]+']);
 
